6b0842c59a0b12cb6194871e8d7cb69de7786a69
MACHINGUNKELLY <117808504+MACHINGUNKELLY@users.noreply.github.com>
2022-12-17 12:34:07 +0300

    Add files via upload

---
+++ дкр 5/дкр5-1.pas
@@ -0,0 +1,18 @@
+﻿const  N=255;
+  type array_type=array [1..N] of integer; 
+ 
+  procedure InsertSort(var x:array_type);
+     var i, j, buf:integer;
+     begin
+         for i:=2 to N do
+         begin
+           buf:=x[i];
+           j:=i-1;
+           while (j>=1) and (x[j]>buf) do
+             begin
+               x[j+1]:=x[j];
+               j:=j-1;
+             end;
+           x[j+1]:=buf;
+         end;
+     end;
---
+++ дкр 5/дкр5-2.pas
@@ -0,0 +1,18 @@
+﻿var a:array [1..100] of integer;
+    x,i,j,n,f: integer;
+begin
+write ('размер масива => '); read (n);
+for i:=1 to n do begin
+write ('Введите член ',i,'-ого значения => '); read (a[i]); end;
+  for i:=2 to n do begin
+  x:=a[i];
+  j:=i-1;
+while (j>0) and (x<a[j]) do begin
+  a[j+1]:=a[j];
+  j:=j-1;
+  end;
+  a[j+1]:=x;
+  end;
+for i:=1 to n do
+write ('a[',i,']=',a[i],'  ');
+end.
